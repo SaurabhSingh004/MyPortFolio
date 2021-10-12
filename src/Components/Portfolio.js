6708f12caa5b1e22a11cb52ee0e60949c4f0a476
Saurabh singh <saurabh.indian98@gmail.com>
2021-10-12 11:53:59 +0530

    first commit

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./Portfolio.css";
 const Portfolio = ({ data }) => {
   if (data) {
     var projects = data.projects.map(function (projects) {
@@ -8,7 +8,7 @@ const Portfolio = ({ data }) => {
         <div key={projects.title} className="columns portfolio-item">
           <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
-              <img alt={projects.title} src={projectImage} />
+              <img alt={projects.title} src={projectImage} className="projimg"/>
               <div className="overlay">
                 <div className="portfolio-item-meta">
                   <h5>{projects.title}</h5>
